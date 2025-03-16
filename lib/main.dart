5294b5e9940db48f45ae495bd8982dc15e07fad1
FadillRvlog24 <150754437+FadillRvlog24@users.noreply.github.com>
2025-03-16 13:09:39 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'register_page.dart';
+import 'main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +22,17 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => LoginPage(),
         '/register': (context) => RegisterPage(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/home') {
+          final int userId =
+              settings.arguments as int; // Ambil userId dari arguments
+          return MaterialPageRoute(
+            builder: (context) =>
+                MainPage(userId: userId), // ✅ Kirim userId ke MainPage
+          );
+        }
+        return null;
       },
     );
   }
